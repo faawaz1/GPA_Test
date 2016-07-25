ac48388fda65a68822ad4ae885ff4997c79723ac
faawaz1 <almobdeea_94@hotmail.com>
2016-07-25 17:24:18 +0300

    size of list

--- app/src/main/java/com/example/pc/test/MainActivity.java
+++ app/src/main/java/com/example/pc/test/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.pc.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,9 +13,11 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,8 +38,6 @@ public class MainActivity extends AppCompatActivity {
     ListView subjectList ;
     SubjectListAdapter subjectListAdapter;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,12 +51,17 @@ public class MainActivity extends AppCompatActivity {
         subjectListAdapter = new SubjectListAdapter(this,R.layout.grades_raw,subjects);
         subjectList.setAdapter(subjectListAdapter);
 
-
-
         btn1= (Button) findViewById(R.id.btn_1);
         txt_1 = (TextView) findViewById(R.id.txt_1);
         r1=(RadioButton)findViewById(R.id.radioButton);
         r2=(RadioButton)findViewById(R.id.radioButton2);
+
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+
+        int deviceHeight = metrics.heightPixels;
+        deviceHeight+=300;
+        ViewGroup.LayoutParams params2 = subjectList.getLayoutParams();
+        params2.height =deviceHeight/3;
 
 
     }
@@ -120,33 +127,30 @@ public class MainActivity extends AppCompatActivity {
             }
     }
 
-    public void calculate(View h){
+    public void calculate(View h) {
 
-        double hours=0.0;
-        double result=0.0;
-        ON=0.0;
-        TW=0.0;
+        double hours = 0.0;
+        double result = 0.0;
+        ON = 0.0;
+        TW = 0.0;
 
-        for (Subject subject:subjects){
-            test(Double.valueOf(subject.getHours()),subject.getGrade());
-            hours+=Double.valueOf(subject.getHours());
+        for (Subject subject : subjects) {
+            test(Double.valueOf(subject.getHours()), subject.getGrade());
+            hours += Double.valueOf(subject.getHours());
         }
 
-        Log.i("Result",String.format("TW = %.2f , Hours = %.2f",TW,hours));
+        Log.i("Result", String.format("TW = %.2f , Hours = %.2f", TW, hours));
 
-        if (hours==0.0){
-            txt_1.setText( "ادخل عدد الساعات اولاً ");
-        }else if(r1.isChecked()) {
+        if (hours == 0.0) {
+            Toast.makeText(getApplicationContext(), "ادخل عدد الساعات اولاً", Toast.LENGTH_LONG).show();
+        } else if (r1.isChecked()) {
 
-            result= TW/hours;
-            txt_1.setText("النقاط: " + TW + "    الساعات: " + hours + "      النسبة: " +  String.format("%.2f", (double)result) + "");
-        }else if (r2.isChecked()){
+            result = TW / hours;
+            txt_1.setText("النقاط: " + String.format("%.1f", (double) TW) + "    الساعات: " + String.format("%.1f", (double) hours) + "      النسبة: " + String.format("%.2f", (double) result) + "");
+        } else if (r2.isChecked()) {
 
-            result= ON/hours;
-            txt_1.setText("النقاط: " + ON + "    الساعات: " + hours + "      النسبة: " +  String.format("%.2f", (double)result) + "");
-        }else
-            txt_1.setText( "اختر نوع النسبة ");
-
+            result = ON / hours;
+            txt_1.setText("النقاط: " + String.format("%.1f", (double) ON) + "    الساعات: " + String.format("%.1f", (double) hours) + "      النسبة: " + String.format("%.2f", (double) result) + "");
+        }
     }
-
 }
